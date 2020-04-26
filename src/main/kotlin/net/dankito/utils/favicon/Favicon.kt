ca48fc05ee73d001c30964bf332a4ff6af29f181
dankito <git@dankito.net>
2020-04-26 16:57:49 +0200

    Refactored and added toString()

--- src/main/kotlin/net/dankito/utils/favicon/Favicon.kt
+++ src/main/kotlin/net/dankito/utils/favicon/Favicon.kt
@@ -3,6 +3,15 @@ package net.dankito.utils.favicon
 import net.dankito.utils.Size
 
 
-data class Favicon(val url : String, val iconType : FaviconType, var size : Size? = null, val type : String? = null) {
+data class Favicon(
+        val url : String,
+        val iconType : FaviconType,
+        var size : Size? = null,
+        val type : String? = null) {
+
+
+    override fun toString(): String {
+        return "$iconType $size $url"
+    }
 
 }
