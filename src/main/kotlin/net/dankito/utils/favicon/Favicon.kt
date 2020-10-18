d2fcb614ae875c4bce2b06f8d5ff62faa0513030
dankito <git@dankito.net>
2020-10-18 06:08:12 +0200

    Made class and properties open

--- src/main/kotlin/net/dankito/utils/favicon/Favicon.kt
+++ src/main/kotlin/net/dankito/utils/favicon/Favicon.kt
@@ -3,11 +3,14 @@ package net.dankito.utils.favicon
 import net.dankito.utils.Size
 
 
-data class Favicon(
-        val url : String,
-        val iconType : FaviconType,
-        var size : Size? = null,
-        val type : String? = null) {
+open class Favicon(
+    open val url : String,
+    open val iconType : FaviconType,
+    open var size : Size? = null,
+    open val type : String? = null
+) {
+
+    constructor() : this("", FaviconType.Icon, null, null) // for object deserializers
 
 
     override fun toString(): String {
