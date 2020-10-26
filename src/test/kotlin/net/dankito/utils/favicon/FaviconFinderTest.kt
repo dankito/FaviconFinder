2d8a3cff412c9d517de6bce9205d24035fd7ea78
dankito <git@dankito.net>
2020-10-26 16:09:40 +0100

    Implemented testing if query has been removed from favicon url

--- src/test/kotlin/net/dankito/utils/favicon/FaviconFinderTest.kt
+++ src/test/kotlin/net/dankito/utils/favicon/FaviconFinderTest.kt
@@ -2,10 +2,17 @@ package net.dankito.utils.favicon
 
 import net.dankito.utils.favicon.web.UrlConnectionWebClient
 import org.assertj.core.api.Assertions.assertThat
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
 import org.junit.jupiter.api.Test
 
 
 class FaviconFinderTest {
+
+    companion object {
+        const val TestSiteUrl = "https://www.test.com"
+    }
+
 
     private val underTest : FaviconFinder = FaviconFinder(UrlConnectionWebClient())
 
@@ -67,6 +74,17 @@ class FaviconFinderTest {
     }
 
     @Test
+    fun psdBankMuenchen_QueryHasBeenRemoved() {
+        val extractedIcons = getFaviconsForUrl("https://www.psd-muenchen.de")
+
+        testExtractedFavicons(extractedIcons, 6)
+
+        extractedIcons.forEach { favicon ->
+            assertThat(favicon.url.contains('?')).isFalse() // check if all queries have been removed from url
+        }
+    }
+
+    @Test
     fun deutscheBank() {
         val extractedIcons = getFaviconsForUrl("https://www.deutsche-bank.de")
 
@@ -74,9 +92,31 @@ class FaviconFinderTest {
     }
 
 
+    @Test
+    fun removeQuery() {
+        val relativeUrlWithoutQuery = "/favicon_196px.png"
+        val relativeUrlWithQuery = "$relativeUrlWithoutQuery?v=1603434540317"
+
+        val extractedIcons = getFaviconsForHtml("<link rel=\"shortcut icon\" href=\"$relativeUrlWithQuery\">")
+
+        testExtractedFavicons(extractedIcons, 1)
+
+        assertThat(extractedIcons.first().url).isEqualTo(TestSiteUrl + relativeUrlWithoutQuery)
+    }
+
+
     private fun getFaviconsForUrl(url: String): List<Favicon> {
         return underTest.extractFavicons(url)
     }
+
+    private fun getFaviconsForHtml(html: String): List<Favicon> {
+        return getFaviconsForDocument(Jsoup.parse(html))
+    }
+
+    private fun getFaviconsForDocument(document: Document): List<Favicon> {
+        return underTest.extractFavicons(document, TestSiteUrl)
+    }
+
 
     private fun testExtractedFavicons(extractedIcons: List<Favicon>, countMinimumFavicons: Int) {
         assertThat(extractedIcons.size).isGreaterThanOrEqualTo(countMinimumFavicons)
