f6464145972a9309d949cd31c97e2b3a47c29d6a
dankito <git@dankito.net>
2024-04-21 23:07:23 +0200

    Creating Jackson modules directly as findAndRegisterModules() doesn't work in native mode

--- src/main/kotlin/net/dankito/utils/favicon/json/JsonSerializer.kt
+++ src/main/kotlin/net/dankito/utils/favicon/json/JsonSerializer.kt
@@ -2,12 +2,13 @@ package net.dankito.utils.favicon.json
 
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 
 object JsonSerializer {
 
     val default: ObjectMapper by lazy {
         ObjectMapper().apply { // do not use jacksonObjectMapper(), it crashes with Kotlin 1.6
-            this.findAndRegisterModules()
+            this.registerModules(KotlinModule.Builder().build()) // don't use findAndRegisterModules(), it won't work in native mode
 
             this.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
         }
