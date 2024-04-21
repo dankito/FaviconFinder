65d8e9a89c39753ec2da9e5719813ee1dd15a889
dankito <git@dankito.net>
2024-04-21 22:28:13 +0200

    Added hint to not use jacksonObjectMapper()

--- src/main/kotlin/net/dankito/utils/favicon/json/JsonSerializer.kt
+++ src/main/kotlin/net/dankito/utils/favicon/json/JsonSerializer.kt
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 object JsonSerializer {
 
     val default: ObjectMapper by lazy {
-        ObjectMapper().apply {
+        ObjectMapper().apply { // do not use jacksonObjectMapper(), it crashes with Kotlin 1.6
             this.findAndRegisterModules()
 
             this.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
