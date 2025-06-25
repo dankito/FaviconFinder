11994430968f9a94994d3fcf81befcdc9bb03f20
dankito <git@dankito.net>
2025-06-25 03:02:43 +0200

    Created FaviconFetcherSelector that selects the best matching FaviconFetcher from a list of FaviconFetchers

---
+++ src/main/kotlin/net/dankito/utils/favicon/fetcher/FaviconFetcherSelector.kt
@@ -0,0 +1,33 @@
+package net.dankito.utils.favicon.fetcher
+
+import net.dankito.utils.favicon.web.IWebClient
+import net.dankito.utils.favicon.web.UrlConnectionWebClient
+
+open class FaviconFetcherSelector(
+    protected val fetchers: Collection<FaviconFetcher> = createDefaultFetchers(UrlConnectionWebClient())
+) {
+
+    companion object {
+        fun createDefaultFetchers(webClient: IWebClient): List<FaviconFetcher> = listOf(
+            GoogleFaviconFetcher(webClient),
+            DuckDuckGoFaviconFetcher(webClient),
+            IconHorseFaviconFetcher(webClient),
+            FaviconExtractorFaviconFetcher(webClient),
+            TwentyIconsFaviconFetcher(webClient),
+            FaviconeFaviconFetcher(webClient),
+            FaviconIsFaviconFetcher(webClient),
+            YandexFaviconFetcher(webClient),
+        )
+    }
+
+
+    constructor(webClient: IWebClient) : this(createDefaultFetchers(webClient))
+
+
+    fun firstMatching(url: String, preferredSize: Int? = null): ByteArray? {
+        val fetchersToQuery = if (preferredSize == null) fetchers else fetchers.filter { it.supportsPreferredSizeParameter }
+
+        return fetchersToQuery.firstNotNullOfOrNull { it.fetch(url, preferredSize) }
+    }
+
+}
