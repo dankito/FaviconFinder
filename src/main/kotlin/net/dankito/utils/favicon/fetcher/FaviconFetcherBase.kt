2555feeaae46041a16ccb0ce4a57800aa350d3d0
dankito <git@dankito.net>
2025-06-25 05:45:25 +0200

    Catching errors

--- src/main/kotlin/net/dankito/utils/favicon/fetcher/FaviconFetcherBase.kt
+++ src/main/kotlin/net/dankito/utils/favicon/fetcher/FaviconFetcherBase.kt
@@ -2,21 +2,28 @@ package net.dankito.utils.favicon.fetcher
 
 import net.dankito.utils.favicon.web.IWebClient
 import net.dankito.utils.favicon.web.UrlUtil
+import org.slf4j.LoggerFactory
 
 abstract class FaviconFetcherBase(protected val webClient: IWebClient, protected val urlUtil: UrlUtil = UrlUtil.Default) : FaviconFetcher {
 
     protected abstract fun getFaviconFetcherUrl(url: String, preferredSize: Int?): String
 
 
-    override fun fetch(url: String, preferredSize: Int?): ByteArray? {
+    private val log = LoggerFactory.getLogger(FaviconFetcherBase::class.java)
+
+
+    override fun fetch(url: String, preferredSize: Int?): ByteArray? = try {
         val fetcherUrl = getFaviconFetcherUrl(url, preferredSize)
         val response = webClient.get(fetcherUrl, true) // actually only required for FaviconExtractor to set the User-Agent (requestDesktopWebsite=true)
 
-        return if (response.successful) {
+        if (response.successful) {
             response.receivedData
         } else {
             null
         }
+    } catch (e: Throwable) {
+        log.error("Could not extract favicon from url '$url'", e)
+        null
     }
 
     protected open fun ensureStartsWithHttpOrHttps(url: String): String =
