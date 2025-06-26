9f34e5484afd19468f4c4d86791ec43860914871
dankito <git@dankito.net>
2025-06-26 03:49:27 +0200

    Extracted method extractIconsFromWebManifest(manifest: WebManifest, manifestAbsoluteUrl: String)

--- src/main/kotlin/net/dankito/utils/favicon/extractor/JacksonWebManifestFaviconsExtractor.kt
+++ src/main/kotlin/net/dankito/utils/favicon/extractor/JacksonWebManifestFaviconsExtractor.kt
@@ -5,6 +5,7 @@ import net.dankito.utils.favicon.Favicon
 import net.dankito.utils.favicon.FaviconType
 import net.dankito.utils.favicon.json.JsonSerializer
 import net.dankito.utils.favicon.webmanifest.WebManifest
+import net.dankito.utils.favicon.webmanifest.WebManifestIcon
 import org.slf4j.LoggerFactory
 import java.net.URL
 
@@ -23,16 +24,20 @@ open class JacksonWebManifestFaviconsExtractor(
     override fun extractIconsFromWebManifest(manifestAbsoluteUrl: String): List<Favicon> = try {
         // don't know why but when requested with URLConnection then web manifest string starts with ﻿ leading to that Jackson deserialization fails
         val manifest = JsonSerializer.default.readValue<WebManifest>(URL(manifestAbsoluteUrl))
-        manifest.icons.mapNotNull {
-            val type = if (it.src.contains("apple-touch", true)) FaviconType.AppleTouch else FaviconType.Icon
-            // a relative icon url is always resolved against manifest's url
-            // TODO: to be standard conformant we should actually check if "Content-Security-Policy: img-src " HTTP header
-            //  is specified for resolving relative icon urls, see https://w3c.github.io/manifest/#content-security-policy
-            creator.createFaviconFromSizesString(it.src, manifestAbsoluteUrl, type, it.type, it.sizes)
-        }
+
+        extractIconsFromWebManifest(manifest, manifestAbsoluteUrl)
     } catch (e: Throwable) {
         log.error("Could not read icons from web manifest of url '$manifestAbsoluteUrl'", e)
         emptyList()
     }
+
+    override fun extractIconsFromWebManifest(manifest: WebManifest, manifestAbsoluteUrl: String): List<Favicon> = manifest.icons.mapNotNull {
+        val type = if (it.src.contains("apple-touch", true)) FaviconType.AppleTouch else FaviconType.Icon
+        // a relative icon url is always resolved against manifest's url
+        // TODO: to be standard conformant we should actually check if "Content-Security-Policy: img-src " HTTP header
+        //  is specified for resolving relative icon urls, see https://w3c.github.io/manifest/#content-security-policy
+        creator.createFaviconFromSizesString(it.src, manifestAbsoluteUrl, type, it.type, it.sizes)
+    }
+
 
 }
--- src/main/kotlin/net/dankito/utils/favicon/extractor/WebManifestFaviconsExtractor.kt
+++ src/main/kotlin/net/dankito/utils/favicon/extractor/WebManifestFaviconsExtractor.kt
@@ -1,9 +1,12 @@
 package net.dankito.utils.favicon.extractor
 
 import net.dankito.utils.favicon.Favicon
+import net.dankito.utils.favicon.webmanifest.WebManifest
 
 interface WebManifestFaviconsExtractor {
 
     fun extractIconsFromWebManifest(manifestAbsoluteUrl: String): List<Favicon>
+
+    fun extractIconsFromWebManifest(manifest: WebManifest, manifestAbsoluteUrl: String): List<Favicon>
 
 }
